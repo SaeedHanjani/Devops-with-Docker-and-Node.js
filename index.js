20dc5499b32b60ee6b11a24a22523dc0a01dc226
Saeed <s.mehdizadehh@gmail.com>
2025-07-14 19:53:34 +0000

    nn

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
+
+mongoose
+    .connect("mongodb://root:example@172.18.0.3:27017/?authSource=admin")
+    .then(() => console.log("succesfully connected to DB"))
+    .catch((e) => console.log(e));
+
+
 
 app.get("/", (req, res) =>{
     res.send("<h2>Hi There!!</h2>");
